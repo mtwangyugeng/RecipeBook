09aabfdef98886740560776710b9548102f26c48
Simon Wang <mtwangyugeng@gmail.com>
2022-03-18 12:39:33 -0400

    css update

---
+++ client/src/lib/recipebook/layouts/ingredients/IngredientCard.svelte
@@ -0,0 +1,48 @@
+<script>
+    export let id;
+    export let name;
+    export let unit;
+    export let best_market;
+    export let common_quantity;
+    export let common_price;
+
+    // TODO: 
+    // export let created_at;
+    // export let updated_at;
+    // export let user_id;
+</script>
+
+
+<div class="container">
+    <h3>#{id} {name}</h3>
+    <div class="img-container"></div>
+    <p>Best Shop: {best_market}</p>
+    <p>Pakage: {common_quantity} {unit} / ${common_price/100}</p>
+</div>
+
+
+<style>
+    .container {
+        background-color: white;
+        width: 200px;
+        height: 260px;
+        margin: 10px;
+        border-radius: 10px;
+
+        padding: 10px;
+        text-transform: capitalize;
+
+        -webkit-box-shadow: 8px 5px 8px -8px #000000; 
+        box-shadow: 8px 5px 8px -8px #000000;
+    }
+
+    h3 {
+        text-align: center;
+    }
+
+    .img-container {
+        height: 50%;
+        border: 1PX solid #000000;
+        margin: 10px 0 15px 0;
+    }
+</style>
--- client/src/lib/recipebook/layouts/ingredients/Ingredients.svelte
+++ client/src/lib/recipebook/layouts/ingredients/Ingredients.svelte
@@ -4,16 +4,34 @@
 
     import PostIngredient from '../../forms/PostIngredient.svelte'
 
+    import IngredientCard from './IngredientCard.svelte'
+
     let addingIngredient = false;
 </script>
 
 
 <section>
     {JSON.stringify($ingredients)}
-
+    <div class="card-container">
+        {#each $ingredients as ingredient (ingredient.id)}
+            <IngredientCard {...ingredient} />
+        {/each}
+    </div> 
     <button on:click={()=>{addingIngredient=true}}>Add Ingredient</button>   
 </section>
 
 {#if addingIngredient}
     <PostIngredient close={()=>{addingIngredient=false}}/>
 {/if}
+
+<style>
+    section {
+        background-color: lightgreen;
+    }
+
+    .card-container {
+        background-color: blue;
+        display: flex;
+        flex-wrap: wrap;
+    }
+</style>
